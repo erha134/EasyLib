23507367d2f1110d044614ba0a067ad3132d9ea0
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2025-02-05 00:35:08 +0800

    Update StreamUtils.java

--- src/main/java/io/github/erha134/easylib/stream/StreamUtils.java
+++ src/main/java/io/github/erha134/easylib/stream/StreamUtils.java
@@ -2,7 +2,9 @@ package io.github.erha134.easylib.stream;
 
 import java.lang.reflect.Array;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,5 +20,15 @@ public class StreamUtils {
     @SuppressWarnings("unchecked")
     public static <T> T[] toArray(Stream<T> stream, Class<T> clazz) {
         return stream.toArray(i -> (T[]) Array.newInstance(clazz, i));
+    }
+
+    public static <K, V> Map<K, V> toMap(Stream<? extends Map.Entry<K, V>> stream) {
+        return toMap(stream, Map.Entry::getKey, Map.Entry::getValue);
+    }
+
+    public static <T, K, V> Map<K, V> toMap(Stream<T> stream,
+                                            Function<? super T, ? extends K> keyMapper,
+                                            Function<? super T, ? extends V> valueMapper) {
+        return stream.collect(Collectors.toMap(keyMapper, valueMapper));
     }
 }
